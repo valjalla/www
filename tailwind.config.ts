975a01b98d83f64d9ff4814116962b2b71c9b5b7
cokefryburger <122137054+cokefryburger@users.noreply.github.com>
2025-03-01 14:34:45 -0500

    rand

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,10 @@ export default {
         naranja: "#ff4800",
         gris: "#aaaaaa",
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [],
